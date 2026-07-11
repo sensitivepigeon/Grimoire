f4e6c5352d66b5fdd3afe1ccf61666f4d8d62ac9
Pigeon <pigeon.ingradients@gmail.com>
2026-07-11 18:32:57 -0400

    text fixes and actual recipe fix

--- src/main/java/grimoire/modid/client/HelpText.java
+++ src/main/java/grimoire/modid/client/HelpText.java
@@ -2,18 +2,20 @@ package grimoire.modid.client;
 
 // this is where I edit help text before future me fixes it in 1.1
 public class HelpText {
-    public static final String LEFT = "Hello, you." +
+    public static final String LEFT = "§l Hello, you." +
             "\n" +
-            "I see you've found the Book. No, don't close it -- I am addressing you. Yes, you." +
+            "I see you've found the Book. No, don't close it -- I am addressing you. Yes,§n you." +
             "\n" +
             "Who am I? I am the notary. The factor. The broker. It's all a matter of language to us bookworms, so feel free to experiment with a little lingo. My job is to manage the requests of valuable patrons. Your job is now to fulfill them, should you choose to accept such a bargain.";
     public static final String RIGHT = "Each day, I will offer you an array of fitting bargains to how much reputation you've accumulated among the patrons. You have one chance to refresh the offers each of those days, the patrons get stiff if you're too indecisive." +
             "\n" +
             "To earn more esteem, merely fulfill the bargains of the patrons you've been offered enough times and you'll be promoted. I will personally recommend you to the next batch of wanting parties." +
             "\n" +
-            "Oh, and bargains will repeat as if they're the same request sometimes. Don't worry about that. Don't think about it. That's not your job." +
+            "Oh, and bargains will repeat as if they're the same request sometimes. Don't worry about that. Don't think about it. §n That's not your job." +
             "\n" +
-            "Let's begin.";
+            "Let us begin, shall we?" +
+            "\n" +
+            "§n- The Broker";
 }
 
 
