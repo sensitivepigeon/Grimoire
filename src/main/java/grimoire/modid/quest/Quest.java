a2281d9f5fc1f0ed2b1f888cf9c7af019ef6e38c
Sensitivepigeon <pigeon.ingradients@gmail.com>
2026-07-13 10:35:07 -0400

    weird internal ordering was bad, fixed

--- src/main/java/grimoire/modid/quest/Quest.java
+++ src/main/java/grimoire/modid/quest/Quest.java
@@ -2,7 +2,7 @@ package grimoire.modid.quest;
 
 import net.minecraft.item.Item;
 
-public record Quest(String id, String title, String lore, String description, int tier, int format, String patron,
+public record Quest(String id, String title, String lore, String description, int tier, String patron, int format,
                     Item requiredItem, int requiredCount,
                     Item rewardItem, int rewardCount) {
 }
